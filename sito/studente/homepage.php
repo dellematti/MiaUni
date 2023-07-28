9a27cc3bd2e523abc29085a6cc7b15f9385bc471
dellematti <116433753+dellematti@users.noreply.github.com>
2023-07-29 00:56:43 +0200

    update

--- sito/studente/homepage.php
+++ sito/studente/homepage.php
@@ -5,7 +5,7 @@
 <head>
 
     <meta charset="utf-8">
-    <title>Unimia</title>
+    <title>UniEuro</title>
 
     <!-- Main Stylesheet -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
@@ -20,11 +20,9 @@
 
 
 <body >
-    <?php
-    echo dirname(__FILE__)
-    ?>
 
-    <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top ">
+    <!-- <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top "> -->
+    <nav class="navbar navbar-expand-sm bg-light navbar-light fixed-top ">
         <div class="container-fluid">
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
                 aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -33,7 +31,7 @@
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0"> 
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="/">Unimia</a>
+                        <a class="nav-link active" id="uni" aria-current="page" href="/">Uni<span id ="euro">Euro</span></a>
                     </li>
                 </ul>
             </div>
@@ -45,51 +43,47 @@
         <div class="main-content">
             <div class="container pt-4 mt-5">
                 <div class="row justify-content-between">
-                        <h2>card studente </h2>
-                            <div class="row top-buffer-s right-buffer" id="">
+                        <?php
+                        session_start();
+                        echo "<h2 class=\"top-buffer-s\">",$_SESSION['email'],"</h2>"
+                        ?>
+                            <div class="row top-buffer right-buffer" id="">
                                 <div class="col-4  bottom-buffer" id="">
-                                    <div class="card cardSelezionabile bg-dark p-3 h-100">
-                                        <img class="card-img    bg-dark" src="" alt="Card image cap">
+                                    <!-- <div class="card cardSelezionabile bg-dark p-3 h-100"> -->
+                                    <div class="card cardSelezionabile bg p-3 h-100">
                                         <div class="card-body">
-                                            <h5 class="card-title">titolo</h5>
+                                            <h5 class="card-title">Iscrizione esami</h5>
+                                            <img class="card-img    bg" src="https://prog2.di.unimi.it/_images/prog2.png" alt="Card image cap">
                                             <a id="albumLink" class="stretched-link" title="Click to do something"
                                                 href="" action="risultati" method="GET">
                                             </a>
-                                            <!-- <a class="link text-white px-0 card-text second-link" href="">altro</a> -->
-                                        </div>
-                                        <div class="card-footer">
-                                            <small class="text-muted">footer</small>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="col-4  bottom-buffer" id="">
-                                    <div class="card cardSelezionabile bg-dark p-3 h-100">
-                                        <img class="card-img    bg-dark" src="" alt="Card image cap">
+                                    <div class="card cardSelezionabile bg p-3 h-100">
                                         <div class="card-body">
-                                            <h5 class="card-title">titolo</h5>
+                                            <h5 class="card-title">La mia carriera</h5>
+                                            <img class="card-img    bg" src="https://www.vigilanzatv.it/wp-content/uploads/2021/01/The-Truman-Show-2.jpg" alt="Card image cap">
                                             <a id="albumLink" class="stretched-link" title="Click to do something"
                                                 href="" action="risultati" method="GET">
                                             </a>
-                                            <!-- <a class="link text-white px-0 card-text second-link" href="">altro</a> -->
-                                        </div>
-                                        <div class="card-footer">
-                                            <small class="text-muted">footer</small>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="col-4  bottom-buffer" id="">
-                                    <div class="card cardSelezionabile bg-dark p-3 h-100">
-                                        <img class="card-img    bg-dark" src="" alt="Card image cap">
+                                    <div class="card cardSelezionabile bg p-3 h-100">
                                         <div class="card-body">
-                                            <h5 class="card-title">titolo</h5>
+                                            <h5 class="card-title">Informazioni utente</h5>
+                                            <img class="card-img    bg" src="https://eyestudios.it/wp-content/uploads/2021/04/9-Consigli-per-Migliorare-lEsperienza-Utente-1024x629.jpg" alt="Card image cap">
                                             <a id="albumLink" class="stretched-link" title="Click to do something"
                                                 href="" action="risultati" method="GET">
                                             </a>
                                             <!-- <a class="link text-white px-0 card-text second-link" href="">altro</a> -->
                                         </div>
-                                        <div class="card-footer">
+                                        <!-- <div class="card-footer">
                                             <small class="text-muted">footer</small>
-                                        </div>
+                                        </div> -->
                                     </div>
                                 </div>
 
