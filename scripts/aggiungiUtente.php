83534789133e4bb8ef6aa72a471ff69497e5dbab
dellematti <116433753+dellematti@users.noreply.github.com>
2023-08-06 23:36:58 +0200

    update

--- scripts/aggiungiUtente.php
+++ scripts/aggiungiUtente.php
@@ -3,13 +3,6 @@
 session_start();
 
 
-// $pdo = require 'connessioneDatabase.php';
-// if ($pdo) {
-//     echo "Connected to the database successfully!";
-// } 
-
-// print_r($_POST);
-
 if (isset($_POST["nome"], $_POST["cognome"], $_POST["tipoUtente"] )) {   // controllo se ci sono nome e cognome e tipo utente
 
     // if ($_POST["tipoUtente"] == "studente" && ! isset($_POST["cdl"])  ) {
@@ -20,16 +13,10 @@ if (isset($_POST["nome"], $_POST["cognome"], $_POST["tipoUtente"] )) {   // cont
         // errore, manca il campo ufficio
     }
     // se l esecuzione non si è fermata prima per gli errori che aggiungerò nelgi if, allora:
-    // devo generare i vari id/matricola e l email giusta (devo controllare che non esistano omonimi)
-
-
-    // sarebbe comodo fare una query e vedere quanti mattia delledonne esistono tra studenti
-
 
     $pdo = require 'C:\xampp\htdocs\unimia\scripts\connessioneDatabase.php';
     
     $query;
-    // se l utente è studente allora la query sarà questa
     if ($_POST["tipoUtente"] == "Studente")
         $query = "SELECT count(*) 
         FROM unieuro.studenti AS s
@@ -57,8 +44,59 @@ if (isset($_POST["nome"], $_POST["cognome"], $_POST["tipoUtente"] )) {   // cont
     if($omonimi != 0) $email = $email . $omonimi;   //se esistono omonimi modifico l email aggiungendo il numero
     if ($_POST["tipoUtente"] == "Studente") $email = $email . "@studente.unimi.it";
     if ($_POST["tipoUtente"] == "Docente") $email = $email . "@docente.unimi.it";
+    
+    $password = 'password';
+ 
+    // ora ho nome cognome email e password (la password potrei generla casualmente, per comodità saranno tutte 'password)
+    // per sapere l id utente dovrò fare un count di utenti, stessa cosa per la matricola se l utente è studente
 
-    echo $email;
+
+    $query = "SELECT count(*) AS numero_utenti
+    FROM unieuro.utenti ";
+    $data = $pdo->query($query); 
+    $idUtente;   
+    foreach($data as $row) {     // devo fare ogni volta sta cosa, se so già che ho una row sola?
+        $idUtente = $row['numero_utenti'] + 1;
+    }
+
+    echo$idUtente,"<br>";
+    echo $email,"<br>";
+    echo $password,"<br>";
+    echo$_POST["nome"],"<br>"; 
+    echo$_POST["cognome"],"<br><br>";
+
+
+    if ($_POST["tipoUtente"] == "Studente") {
+        $query = "SELECT count(*) 
+        FROM unieuro.studenti ";
+        $data = $pdo->query($query); 
+        $matricola;   
+        foreach($data as $row) {     // devo fare ogni volta sta cosa, se so già che ho una row sola?
+            $matricola = $row['count'] + 1;
+        }
+        echo$matricola,"<br>";
+        echo$_POST["cdl"],"<br>";
+        echo$idUtente,"<br>";
+
+        $query = "BEGIN TRAN aggiungiStudente
+        INSERT INTO unieuro.utenti 
+        VALUES ({$idUtente}, '{$email}', '{$password}', '{$_POST['nome']}', '{$_POST['cognome']}')
+        INSERT INTO unieuro.studenti 
+        VALUES ({$matricola}, {$_POST['cdl']}, {$idUtente})           
+        COMMIT TRAN aggiungiStudente";
+        $data = $pdo->query($query); 
+
+        // e inserisco utente e studente al db
+    }
+
+    if ($_POST["tipoUtente"] == "Docente") {
+        echo$idUtente,"<br>";
+        echo$_POST["ufficio"],"<br>";
+        // e inserisco tutto nel database
+    }
+
+    // dovrò fare in modo di poter aggiungere anche utenti della segreteria
+
 
 }
 
