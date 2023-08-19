a621aaa8f9fdb719cc2e0ed4258d204eb91ece4b
dellematti <116433753+dellematti@users.noreply.github.com>
2023-08-19 18:59:22 +0200

    update

---
+++ scripts/docente/aggiungiEsito.php
@@ -0,0 +1,16 @@
+<?php
+    session_start();
+
+    // dovrei controllare, magari con un trigger, che l esito che sto inserendo non sia già esistente
+
+    // print_r($_SESSION);
+    print_r($_POST);
+
+    if (isset($_POST["appello"], $_POST["matricola"], $_POST["esito"] )) { 
+        $pdo = require 'C:\xampp\htdocs\unimia\scripts\connessioneDatabase.php';
+        $query = "INSERT INTO unieuro.studentiesami 
+        VALUES ({$_POST["matricola"]}, {$_POST["appello"]}, '{$_POST["esito"]}')";
+        $data = $pdo->query($query); 
+    }
+    header('Location: http://localhost/unimia/sito/docente/homepage_docente.php');
+?>
