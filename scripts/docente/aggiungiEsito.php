54ad3b6fb49dc34996ee413a314bc7b93e901864
dellematti <116433753+dellematti@users.noreply.github.com>
2023-09-07 17:24:27 +0200

    update

--- scripts/docente/aggiungiEsito.php
+++ scripts/docente/aggiungiEsito.php
@@ -6,10 +6,13 @@
     print_r($_POST);
 
     if (isset($_POST["appello"], $_POST["matricola"], $_POST["esito"] )) { 
-        $pdo = require 'C:\xampp\htdocs\unimia\scripts\connessioneDatabase.php';
-        $query = "INSERT INTO unieuro.studentiesami 
-        VALUES ({$_POST["matricola"]}, {$_POST["appello"]}, '{$_POST["esito"]}')";
-        $data = $pdo->query($query); 
+        require 'C:\xampp\htdocs\unimia\scripts\connessioneDatabase2.php';
+        $dbConnect = openConnection();
+                                                
+        $query = "CALL inserire_voto ( $1, $2, $3)";
+        $res = pg_prepare($dbConnect, "", $query);
+        $row = pg_fetch_all(pg_execute($dbConnect, "", array($_POST["matricola"], $_POST["appello"],  $_POST["esito"]  )));
+
     }
     header('Location: http://localhost/unimia/sito/docente/homepage_docente.php');
 ?>
