fa0619e78830721251cbd5b92d57836cb693df15
dellematti <116433753+dellematti@users.noreply.github.com>
2023-09-10 17:33:42 +0200

    update

--- scripts/studente/aggiungiIscrizione.php
+++ scripts/studente/aggiungiIscrizione.php
@@ -4,14 +4,19 @@
     print_r($_SESSION);
     print_r($_POST);
 
-    if (isset($_SESSION["matricola"], $_POST["appello"] )) { 
+    // controllo che l utente non abbia messo sia un esame del suo cdl sia uno di un altro cdl
+    if (isset($_SESSION["matricola"], $_POST["appello"]  ) xor isset($_SESSION["matricola"], $_POST["appelliNonCDL"]  ) ) { 
         echo"ciao";
         require 'C:\xampp\htdocs\unimia\scripts\connessioneDatabase2.php';
         $dbConnect = openConnection();
                                                 
         $query = "CALL iscrizione_appello ( $1, $2)";
         $res = pg_prepare($dbConnect, "", $query);
-        $row = pg_fetch_all(pg_execute($dbConnect, "", array($_SESSION["matricola"], $_POST["appello"] )));
+        if (isset($_POST["appello"]))
+            $row = pg_fetch_all(pg_execute($dbConnect, "", array($_SESSION["matricola"], $_POST["appello"] )));
+        else 
+            $row = pg_fetch_all(pg_execute($dbConnect, "", array($_SESSION["matricola"], $_POST["appelliNonCDL"] )));
+
 
     }
     header('Location: http://localhost/unimia/sito/studente/homepage.php');
