374f2ea0a4e0ef6e3d023df90ccbadcdc859a51a
dellematti <116433753+dellematti@users.noreply.github.com>
2023-09-19 11:47:46 +0200

    update

--- scripts/segreteria/aggiungiUtente.php
+++ scripts/segreteria/aggiungiUtente.php
@@ -44,7 +44,7 @@ if (isset($_POST["nome"], $_POST["cognome"], $_POST["tipoUtente"] )) {   // cont
     $email = $nomeUtente . '.' . $cognomeUtente;
     if($omonimi != 0) $email = $email . $omonimi;   //se esistono omonimi modifico l email aggiungendo il numero
     if ($_POST["tipoUtente"] == "Studente") $email = $email . "@studenti.unimi.it";
-    if ($_POST["tipoUtente"] == "Docente") $email = $email . "@docente.unimi.it";
+    if ($_POST["tipoUtente"] == "Docente") $email = $email . "@docenti.unimi.it";
     
     $password = 'password';
  
